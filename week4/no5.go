2dbe4e03e7b06bfdb5a09066c40c701f0f54f693
unemployed jia <dziahaqi233@gmail.com>
2025-10-12 14:26:20 +0700

    update week4 no.5

--- week4/no5.go
+++ week4/no5.go
@@ -1,33 +1,50 @@
 package main
-
 import "fmt"
 
 func main() {
-	var hBeli, hJual, jSaham, tInvest, tPenjualan, kKotor, bTransaksi, pKeuntungan, kBersih float64
+    type saham struct {
+        beli     float64
+        jual     float64
+        jSaham   int
+        tinvest  float64
+        tjual    float64
+        kotor    float64
+        biaya    float64
+        pajak    float64
+        bersih   float64
+    }
 
-	fmt.Print("Harga Beli per Saham (Rp): ")
-	fmt.Scan(&hBeli)
-	fmt.Print("Harga Jual per Saham (Rp): ")
-	fmt.Scan(&hJual)
-	fmt.Print("Jumlah Saham (lembar): ")
-	fmt.Scan(&jSaham)
-	fmt.Println()
+    var s saham
+    
+    fmt.Print("Harga beli saham per lembar: ")
+    fmt.Scan(&s.beli)
+    fmt.Print("Harga jual saham per lembar: ")
+    fmt.Scan(&s.jual)
+    fmt.Print("Jumlah saham: ")
+    fmt.Scan(&s.jSaham)
 
-	tInvest = hBeli * jSaham
-	tPenjualan = hJual * jSaham
-	kKotor = tPenjualan - tInvest
-	bTransaksi = tPenjualan * (0.2 / 100)
-	pKeuntungan = 0.0
-	pKeuntungan = (map[bool]float64{true: kKotor * 0.1})[kKotor > 0]
-	kBersih = kKotor - bTransaksi - pKeuntungan
+    s.tinvest = s.beli * float64(s.jSaham)
+    s.tjual = s.jual * float64(s.jSaham)
+    s.kotor = s.tjual - s.tinvest
+    s.biaya = s.tjual * 0.002
 
-	fmt.Printf("Harga Beli per Saham\t: Rp %.2f\n", hBeli)
-	fmt.Printf("Harga Jual per Saham\t: Rp %.2f\n", hJual)
-	fmt.Printf("Jumlah Saham\t\t: %.0f lembar\n", jSaham)
-	fmt.Printf("Total Investasi\t\t: Rp %.2f\n", tInvest)
-	fmt.Printf("Total Penjualan\t\t: Rp %.2f\n", tPenjualan)
-	fmt.Printf("Keuntungan Kotor\t: Rp %.2f\n", kKotor)
-	fmt.Printf("Biaya Transaksi\t\t: Rp %.2f\n", bTransaksi)
-	fmt.Printf("Pajak Keuntungan\t: Rp %.2f\n", pKeuntungan)
-	fmt.Printf("Keuntungan Bersih\t: Rp %.2f\n", kBersih)
+    if s.kotor > 0 {
+        s.pajak = s.kotor * 0.1
+    } else {
+        s.pajak = 0
+    }
+    s.bersih = s.kotor - s.biaya - s.pajak
+
+    
+    fmt.Println("\n=== LAPORAN TRANSAKSI SAHAM ===")
+    fmt.Printf("Harga Beli per Saham\t: Rp %.2f\n", s.beli)
+    fmt.Printf("Harga Jual per Saham\t: Rp %.2f\n", s.jual)
+    fmt.Printf("Jumlah Saham\t\t: %d lembar\n", s.jSaham)
+    fmt.Printf("Total Investasi\t\t: Rp %.2f\n", s.tinvest)
+    fmt.Printf("Total Penjualan\t\t: Rp %.2f\n", s.tjual)
+    fmt.Printf("Laba/Rugi Kotor\t\t: Rp %.2f\n", s.kotor)
+    fmt.Printf("Biaya Transaksi (0.2%%)\t: Rp %.2f\n", s.biaya)
+    fmt.Printf("Pajak (10%%)\t\t: Rp %.2f\n", s.pajak)
+    fmt.Println("---------------------------------")
+    fmt.Printf("Laba/Rugi Bersih\t: Rp %.2f\n", s.bersih)
 }
